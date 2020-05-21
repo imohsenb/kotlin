22948e35c23ce1c85b7ff4d9b79867eae8821cda
Mohsen Beiranvand <imohsen.b@gmail.com>
2020-05-21 21:20:24 +0430

    set data into mutable live data

--- app/src/main/java/com/imohsenb/kotlin/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/imohsenb/kotlin/viewmodel/MainViewModel.kt
@@ -2,7 +2,9 @@
 package com.imohsenb.kotlin.viewmodel
 
 import android.annotation.SuppressLint
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.imohsenb.kotlin.model.GalleryModel
 import com.imohsenb.kotlin.repository.GalleryRepository
 import com.imohsenb.kotlin.transformer.schedulersTransformer
 import javax.inject.Inject
@@ -11,11 +13,18 @@ class MainViewModel @Inject constructor(
     private val galleryRepository: GalleryRepository
 ) : ViewModel() {
 
+    val galleryList: MutableLiveData<List<GalleryModel>> by lazy {
+        MutableLiveData()
+    }
+
     @SuppressLint("CheckResult")
     fun loadData() {
         galleryRepository.getGallery()
             .compose(schedulersTransformer())
             .subscribe { data, throwable ->
+                data?.let {
+                    galleryList.postValue(it)
+                }
                 throwable?.printStackTrace()
             }
     }
